72474ab51e73ec971030c06748b15ddff36588b2
Melissa Real <melissa.real@hotmail.com>
2019-02-10 12:48:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def newif params[:author_id] && !Author.exists?(params[:author_id])
-    @post = Post.new(author_id: params[:author_id])
+  def new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end 
   end
 
   def create
